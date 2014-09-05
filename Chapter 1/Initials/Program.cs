4e5bbfe7066328b2fb9923268af97f6ed42bc315
fulsel <logan.fulse10@gmail.com>
2014-09-05 08:28:23 -0400

    Finished Initials
    Completed pg. 42 problems #4-8.

---
+++ Chapter 1/Initials/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Initials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("L           FFFFFFFF ");
+            Console.WriteLine("L           F");
+            Console.WriteLine("L           FFFFF");
+            Console.WriteLine("L           F");
+            Console.WriteLine("L           F");
+            Console.WriteLine("LLLLLLL     F");
+        }
+    }
+}
